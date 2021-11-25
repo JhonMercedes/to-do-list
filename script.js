aeeefce8f71076800847a74456be98cd298f6803
Jhon Armando <jhonzeras93@gmail.com>
2021-11-25 17:37:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelector('#push').onclick = function (){
+    if(document.querySelector('#novatask input').value.length == 0){
+        alert("Por favor insira uma task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += 
+        `<div class="task">
+            <span id="tasknome">
+                    ${document.querySelector('#novatask input').value}
+            </span>
+            <button class="delete">
+                <i class="far fa-trash-alt">Delete</i>
+            </button>
+        </div>
+    `;
+    var current_tasks = document.querySelectorAll(".delete");
+    for(var i=0; i<current_tasks.length; i++) {
+        current_tasks[i].onclick = function(){
+            this.parentNode.remove();
+        }
+    }
+    var tasks = document.querySelectorAll(".task");
+    for(var i = 0; i<tasks.length; i++) {
+        tasks[i].onclick = function(){
+            this.classList.toggle("completed")
+        }
+    }
+
+    document.querySelector("#novatask input").value = "";
+    }
+}
